7471c25186026d22e688b3e9e409c1d6d6c00004
Jorge P. OConnor <jorgepoconnor@gmail.com>
2019-09-13 16:09:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,15 @@
 function happyHolidays(string) {
   return "Happy holidays!";
 }
+
+function happyHolidaysTo(name) {
+  return "Happy holidays, ${name}!";
+}
+
+function happyHolidaysTo(holiday, name) {
+  return "happy ${holiday}, ${name}!";
+}
+
+function holidayCountdown(holiday, days) {
+  return "it's ${days} days until ${holiday}!";
+}
